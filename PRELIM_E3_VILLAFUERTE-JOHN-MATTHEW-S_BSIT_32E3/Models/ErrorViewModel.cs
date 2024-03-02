623e40127a31a1b64f3a983b7d867f0cb6f40635
Shin <Shin@SHIN>
2024-03-02 20:34:23 +0800

    Add project files.

---
+++ PRELIM_E3_VILLAFUERTE-JOHN-MATTHEW-S_BSIT_32E3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_VILLAFUERTE_JOHN_MATTHEW_S_BSIT_32E3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
